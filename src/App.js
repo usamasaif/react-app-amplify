264901c78b446d6126a119383b81063518dcce00
usamasaif <usamasaif350@gmail.com>
2025-09-30 13:21:01 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is APP 1.</p>
+        <p>This is APP 1.Hello from AWS Amplify</p>
         <a
           className="App-link"
           href="https://reactjs.org"
